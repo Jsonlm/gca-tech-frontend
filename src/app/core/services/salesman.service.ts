a6be9ebd60bd96620262f5f4a242ddef906ec91b
Jsonlm <jlenism@outlook.com>
2024-02-16 14:35:13 -0500

    ajuste headers para consulta

--- src/app/core/services/salesman.service.ts
+++ src/app/core/services/salesman.service.ts
@@ -1,7 +1,12 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Salesman } from '../models/salesman.model.ts';
+
+const headers= new HttpHeaders()
+.set('Access-Control-Allow-Headers', 'X-API-KEY,Origin,X-Requested-With,Content-Type,x-access-token,authorization,Authorization,Accept,Access-Control-Request-Method,token, application/json' )
+.set('Access-Control-Allow-Origin', 'http://74.235.109.154/*')
+.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
 @Injectable({
   providedIn: 'root'
@@ -12,14 +17,14 @@ export class SalesmanService {
   constructor(private http: HttpClient) { }
 
   getAll(): Observable<Salesman> {
-    return this.http.get<Salesman>(`${this.url}/salesman`)
+    return this.http.get<Salesman>(`${this.url}/salesman`, {headers: headers})
   }
   
   getById(id: string): Observable<Salesman> {
-    return this.http.get<Salesman>(`${this.url}/salesman/${id}`)
+    return this.http.get<Salesman>(`${this.url}/salesman/${id}`, {headers: headers})
   }
   
   create(salesman: Salesman): Observable<Salesman> {
-    return this.http.post<Salesman>(`${this.url}/salesman`, salesman)
+    return this.http.post<Salesman>(`${this.url}/salesman`, salesman , {headers: headers})
   }
 }
